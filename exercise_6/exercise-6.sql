f1e1a85e3da30ac3ea28c4347a022041fe4b10de
aidanliddiard <aidanliddiard@gmail.com>
2022-06-15 16:37:33 -0700

    exercise 6 passing

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -2,5 +2,7 @@
 -- Similar to exercise 5
 SELECT title
 FROM film
-LEFT JOIN 
+LEFT JOIN inventory on inventory.film_id = film.film_id
+LEFT JOIN rental on inventory.inventory_id = rental.inventory_id
+LEFT JOIN customer on rental.customer_id = customer.customer_id
 WHERE customer.first_name='Roberta'
